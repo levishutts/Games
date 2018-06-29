46f2a305f8dce8929fee546fc24f6706aeb7101e
levishutts <levicshutts@hotmail.com>
2018-06-29 14:26:13 -0600

    Better camera angle and accelration/deceleration
    Next add mini cam in the corner on just jenga blocks

--- Jenga/Assets/Scripts/CameraController.cs
+++ Jenga/Assets/Scripts/CameraController.cs
@@ -7,15 +7,19 @@ public class CameraController : MonoBehaviour
     public GameObject player;
 
     public float distance;
+    
+    Vector3 offset = new Vector3(0, 0.2f, 0);
 
     void Start()
     {
-        
+        if (Display.displays.Length > 1)
+            Display.displays[2].Activate();
     }
 
     void LateUpdate()
     {
-        transform.position = player.transform.position - (player.transform.forward * distance);
+        transform.position = player.transform.position - (player.transform.forward * distance) + offset;
         transform.LookAt(player.transform);
+        transform.position += offset;
     }
 }
--- Jenga/Assets/Scripts/PlayerController.cs
+++ Jenga/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,11 @@ using System.Collections;
 public class PlayerController : MonoBehaviour
 {
 
-    public float speed;
+    private float speed = 10;
+
+    public float maxSpeed = 20;
+    public float minSpeed = 5;
+    private float totalTime;
 
     private Rigidbody rb;
 
@@ -13,16 +17,30 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
+    private void Update()
+    {
+
+    }
+
     void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Vertical");
         float moveVertical = Input.GetAxis("Horizontal");
 
-        rb.velocity = (transform.forward * speed);
-
         Vector3 input = new Vector3(-moveHorizontal, moveVertical, 0.0f);
-    
+
         rb.transform.Rotate(input);
         rb.transform.Rotate(0, 0, -rb.transform.eulerAngles.z);
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (speed < maxSpeed)
+                speed = (Time.deltaTime * 100) + speed;
+        }
+        else
+            if (speed > minSpeed)
+                speed = speed - (Time.deltaTime * 100);
+
+        rb.velocity = (transform.forward * speed);
     }
 }
