bd76b946c88aa77efa76fcc37ec1ea60606f4067
levishutts <levicshutts@hotmail.com>
2018-07-03 15:11:41 -0600

    Blocks respawn on top of tower after hitting the ground
    Change placement of respawn to be more accurate and avoid spawning collisions

--- Jenga/Assets/Scripts/FloorController.cs
+++ Jenga/Assets/Scripts/FloorController.cs
@@ -6,6 +6,14 @@ public class FloorController : MonoBehaviour {
 
     public Transform block;
 
+    public float waitTime = 1f;
+
+    private float timer;
+
+    private int blockCount;
+    private int height;
+    private char orientation = 'x';
+    
     // Use this for initialization
     void Start()
     {
@@ -21,8 +29,13 @@ public class FloorController : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-		
-	}
+        timer += Time.deltaTime;
+    }
+
+    void wait()
+    {
+
+    }
 
     void OnCollisionEnter(Collision collision)
     {
@@ -31,6 +44,39 @@ public class FloorController : MonoBehaviour {
             collision.gameObject.transform.position = new Vector3 (0f, 10, -50);
             collision.gameObject.transform.rotation = Quaternion.identity;
         }
-            
+
+        if (collision.gameObject.tag == "block")
+        {
+            if (timer < waitTime)
+                return;
+            Debug.Log("block collision with floor " + orientation);
+            Debug.Log("blockCount " + blockCount);
+            Debug.Log("height " + height);
+            collision.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            if (orientation == 'x')
+            {
+                collision.gameObject.transform.position = new Vector3(2 * blockCount - 2, height + 16.5f, 0);
+                collision.gameObject.transform.rotation = Quaternion.Euler(0, 90, 0);
+                blockCount++;
+            }
+
+            if (orientation == 'z')
+            {
+                collision.gameObject.transform.position = new Vector3(0, height + 16.5f, 2 * blockCount - 2);
+                collision.gameObject.transform.rotation = Quaternion.identity;
+                blockCount++;
+            }
+            if (blockCount == 3)
+            {
+                blockCount = 0;
+                if (orientation == 'x')
+                    orientation = 'z';
+                else
+                    orientation = 'x';
+                ++height;
+            }
+
+        }
+
     }
 }
